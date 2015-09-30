96962c0e135956f5f297a1142dcba090b7108870
Samuel Benton <xable@xable.us>
2015-09-30 12:56:44 -0500

    Initial branch commit

--- Provided Examples/04_form_recognition_and_print/form_recognition_and_print.c
+++ Provided Examples/04_form_recognition_and_print/form_recognition_and_print.c
@@ -15,6 +15,30 @@
 #include "Bitmap.h" 
 #include "pattern.h"
 
+#define RUN 0x05
+#define STOP 0
+#define LEFT 0x09
+#define RIGHT 0x06
+#define BACK 0x0A
+
+// Used to initialize the bot motors
+void InitPobProto (void)
+{
+	PobProto	Proto; // struct to set the pob-proto
+	
+	Proto.porta=ALL_PORTA_AS_ANA;	//to get the position of the analogic joystick, you have to set the PORTA as analogic input
+	Proto.portc=RC7_AS_DO| RC6_AS_DO |RC3_AS_DO |RC2_AS_SERVO|RC1_AS_SERVO |RC0_AS_SERVO;	//all pin of PORTC are configured to manage servomotors
+	Proto.portd=RD7_AS_DI	| RD6_AS_DI	|RD5_AS_DI |RD4_AS_DI|RD3_AS_DO	|RD2_AS_DO	|RD1_AS_DO	|RD0_AS_DO;		//RD0 RD1 RD2 RD3 are configured as digitals output to gear DC motor, RD4 RD5 RD6 RD7 are configured as digitals input
+	SetPobProto(&Proto);	//set the pob proto
+}
+
+
+// Used to make the project move
+void MoveBot(UInt8 Way)
+{
+	SetPortD(Way);
+}
+
 int main (void)
 {	
 	UInt8 i=0,Nb_Identify=0;
@@ -35,6 +59,10 @@ int main (void)
 	InitCameraPobeye2();
 	InitLCD();	
 
+
+	InitPobProto();
+	SwitchOnAllServo();
+	
 	// Get the pointer of the red,green and blue video buffer
 	FrameFromCam = GetRGBFrame();
 		
@@ -64,34 +92,39 @@ int main (void)
 				// Draw bitmap on the buffer and the LCD screen
 				DrawBitmap(0,0,IDB_CROSS,bitmap,&ScreenBuffer);
 				DrawLCD(&ScreenBuffer);
+				MoveBot(RUN);
 			break;
 				
-			case IDP_1_BIGA:
-				DrawBitmap(0,0,IDB_BIGA,bitmap,&ScreenBuffer);
-				DrawLCD(&ScreenBuffer);
-			break;
+//			case IDP_1_BIGA:
+//				DrawBitmap(0,0,IDB_BIGA,bitmap,&ScreenBuffer);
+//				DrawLCD(&ScreenBuffer);
+//			break;
 
 			case IDP_2_KING:
 				DrawBitmap(0,0,IDB_KING,bitmap,&ScreenBuffer);
 				DrawLCD(&ScreenBuffer);
+				MoveBot(LEFT);
 			break;
 
-			case IDP_3_TOWER:
-				DrawBitmap(0,0,IDB_TOWER,bitmap,&ScreenBuffer);
-				DrawLCD(&ScreenBuffer);
-			break;
+//			case IDP_3_TOWER:
+//				DrawBitmap(0,0,IDB_TOWER,bitmap,&ScreenBuffer);
+//				DrawLCD(&ScreenBuffer);
+//			break;
 			
-			case IDP_4_TREFLE:
-				DrawBitmap(0,0,IDB_BIGA,bitmap,&ScreenBuffer);
-				DrawLCD(&ScreenBuffer);
-			break;
+//			case IDP_4_TREFLE:
+//				DrawBitmap(0,0,IDB_BIGA,bitmap,&ScreenBuffer);
+//				DrawLCD(&ScreenBuffer);
+//			break;
 			
-			case IDP_5_TRIANGLE:
-				DrawBitmap(0,0,IDB_TRIANGLE,bitmap,&ScreenBuffer);
-				DrawLCD(&ScreenBuffer);
-			break;
+//			case IDP_5_TRIANGLE:
+//				DrawBitmap(0,0,IDB_TRIANGLE,bitmap,&ScreenBuffer);
+//				DrawLCD(&ScreenBuffer);
+//			break;
+			
 			default:
+				MoveBot(STOP);
 			break;
+			
 			}				
 		}		
 	if (Nb_Identify == 0)
