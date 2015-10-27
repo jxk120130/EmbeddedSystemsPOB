dc4452da0f98d2a0c1de9d2b6227df1546e51f87
kyleparrott <kyle.parrott@gmail.com>
2015-10-27 13:49:01 -0500

    motion working but display still messed up - sorry for sloppy code

--- Provided Examples/04_form_recognition_and_print/form_recognition_and_print.c
+++ Provided Examples/04_form_recognition_and_print/form_recognition_and_print.c
@@ -14,12 +14,35 @@
 
 #include "Bitmap.h" 
 #include "pattern.h"
+#include "pad.h"
+
 
 #define RUN 0x05
 #define STOP 0
 #define LEFT 0x09
 #define RIGHT 0x06
 #define BACK 0x0A
+
+#define BUFFER_WIDTH	64
+#define BUFFER_HEIGHT	64
+
+//function to draw picture on both screen, when a form is recognize
+static void DrawWhatIsKnown(Form What);	
+//function to draw rectangle arround the form known
+static UInt8 DrawRect (Rect Rectangle,const GraphicBuffer *Target);	
+//extern void PrintTextOnPobLCD(int row, int col, char *string, UInt8 *Screen_Buffer);
+//extern void InitAsciiBuffer();
+//extern void PrintTextOnPobLCD(int row, int col, char *string, UInt8 *Screen_Buffer);
+
+
+
+
+UInt8 LCD_Left_Buffer [BUFFER_WIDTH*BUFFER_HEIGHT*BYTES];  // array to store the left lcd screen, one byte per pixel
+UInt8 LCD_Right_Buffer [BUFFER_WIDTH*BUFFER_HEIGHT*BITS];  // array to store the right lcd screen one bit per pixel.
+
+GraphicBuffer LCD_Left ;  // left graphic buffer 
+GraphicBuffer LCD_Right ; // right graphic buffer 
+UInt8 X_Pic=5,Y_Pic=5;	  // x,y to draw pictures of form known on the right LCD screen 
 
 // Used to initialize the bot motors
 void InitPobProto (void)
@@ -34,9 +57,33 @@ void MoveBot(UInt8 Way)
 	SetPortD(Way);
 }
 
+void turnRight(){
+	MoveBot(RIGHT);
+	WaitUs(1500000);
+	MoveBot(STOP);
+}
+
+void turnLeft(){
+	MoveBot(LEFT);
+	WaitUs(1500000);
+	MoveBot(STOP);
+}
+
+void runForward(int inches){
+	int time = 237500 * inches;
+	MoveBot(RUN);
+	WaitUs(time);
+	MoveBot(STOP);
+}
+
+void signal(){
+	//do somthing
+}
+
 int main (void)
 {	
-	UInt8 i=0,Nb_Identify = 0, Nb_Last = 0; 
+	UInt8 i=0,Nb_Identify = 0, Nb_Last = 0;
+	UInt8 state = 0;
 	// List of form
 	Form ListOfForm[MAX_OF_FORM];
 
@@ -53,21 +100,32 @@ int main (void)
 	InitCameraPobeye2();
 	InitLCD();	
 	InitPobProto();
+	//InitAsciiBuffer(); 
 	SwitchOnAllServo();
+	SetDCSpeed(5);
+	
+	InitGraphicBuffer( &LCD_Left,BUFFER_WIDTH,BUFFER_HEIGHT,EIGHT_BITS,LCD_Left_Buffer);
+	InitGraphicBuffer( &LCD_Right,BUFFER_WIDTH,BUFFER_HEIGHT,ONE_BIT,LCD_Right_Buffer);
+
+	// Clear the Left and Right screen
+	ClearGraphicBuffer(&LCD_Left); 
+	ClearGraphicBuffer(&LCD_Right);
 	
 	// Get the pointer of the red,green and blue video buffer
 	FrameFromCam = GetRGBFrame();
 		
 	// Init the Graphic buffer with 128 per 64, one pixel per bit and LCD_Buffer
-	InitGraphicBuffer( &ScreenBuffer, LCD_WIDTH,LCD_HEIGHT,ONE_BIT,LCD_Buffer);
+	//InitGraphicBuffer( &ScreenBuffer, LCD_WIDTH,LCD_HEIGHT,ONE_BIT,LCD_Buffer);
 
 	// clear the graphic buffer
-	ClearGraphicBuffer(&ScreenBuffer);
+	//ClearGraphicBuffer(&ScreenBuffer);
 
 	while(1)
 	{
 		// grab the RGB components
-		GrabRGBFrame();				
+		GrabRGBFrame();
+		//PrintTextOnPobLCD(1, 2, "Top", &LCD_Right);
+				
 		
 		// Binary the three RGB Buffer
 		BinaryRGBFrame(FrameFromCam); 
@@ -83,31 +141,43 @@ int main (void)
 			{
 				case IDP_6_CIRCLE:
 					// Draw bitmap on the buffer and the LCD screen
-					DrawBitmap(0,0,IDB_KING,bitmap,&ScreenBuffer);
-					DrawLCD(&ScreenBuffer);
+					DrawBitmap(0,0,IDB_KING,bitmap,&LCD_Left);
+					DrawLeftLCD(&LCD_Left);
 					MoveBot(RUN);
 					// Nb_Last = RUN;
-				break;
+					break;
 
 				case IDP_0_CROSS:
-					DrawBitmap(0,0,IDB_CROSS,bitmap,&ScreenBuffer);
-					DrawLCD(&ScreenBuffer);
-					MoveBot(LEFT);
+					DrawBitmap(0,0,IDB_CROSS,bitmap,&LCD_Left);
+					DrawLeftLCD(&LCD_Left);
+					if(state == 0){
+						turnLeft();
+						state++;					
+					} else if (state == 2){
+						MoveBot(STOP);
+						return; //end of execution.
+					}
+					//MoveBot(LEFT);
 					Nb_Last = LEFT;
-				break;
+					break;
 				
 				case IDP_1_BIGA:
-					DrawBitmap(0,0,IDB_BIGA,bitmap,&ScreenBuffer);
-					DrawLCD(&ScreenBuffer);
-					MoveBot(RUN);
-				break;				
+					DrawBitmap(0,0,IDB_BIGA,bitmap,&LCD_Left);
+					DrawLeftLCD(&LCD_Left);
+					//MoveBot(RUN);
+					break;				
 				
 				case IDP_5_TRIANGLE:
-					DrawBitmap(0,0,IDB_TRIANGLE,bitmap,&ScreenBuffer);
-					DrawLCD(&ScreenBuffer);
-					MoveBot(RIGHT);
+					DrawBitmap(0,0,IDB_TRIANGLE,bitmap,&LCD_Left);
+					DrawLeftLCD(&LCD_Left);
+					if(state == 1){
+						turnRight();
+						state++;
+						signal();
+					}
+					//MoveBot(RIGHT);
 					Nb_Last = RIGHT;
-				break;					
+					break;					
 
 				default:
 				break;
@@ -116,9 +186,12 @@ int main (void)
 		
 		if (Nb_Identify == 0)
 		{
-			DrawBitmap(0,0,IDB_NOFORMS,bitmap,&ScreenBuffer);
-			DrawLCD(&ScreenBuffer);
-			MoveBot(Nb_Last);
+			DrawBitmap(0,0,IDB_NOFORMS,bitmap,&LCD_Left);
+			DrawLeftLCD(&LCD_Left);
+			//MoveBot(Nb_Last);
+			if(state == 2){
+				//return;
+			}
 		}
 	}
 
